8a08acca575cf2116afb113b2cff97ba4f4345b6
Wilson, Xander Cohen <xcwilson@iu.edu>
2021-12-02 16:59:38 -0500

    Milestone 2 Complete
    Everything done but items

--- Evermann/Assets/Scripts/GameController.cs
+++ Evermann/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class GameController : MonoBehaviour
     private int curDoor;
     private int playerCurrentSanity;
     private GameObject outcome;
+    private GameObject itemName;
+    private bool currentlyEnding = false;
 
     [SerializeField] GameObject monster;
     [SerializeField] GameObject item;
@@ -22,9 +25,11 @@ public class GameController : MonoBehaviour
     [SerializeField] AudioSource audioSource;
     [SerializeField] Text doorsLeft;
     [SerializeField] Text outcomeText;
+    [SerializeField] Text damageDesc;
     [SerializeField] Image fadingToBlack;
     [SerializeField] int playerMaxSanity = 100;
     [SerializeField] int monsterDamage = 20;
+    [SerializeField] BoxCollider blockDoorsCollider;
 
 
     // Start is called before the first frame update
@@ -36,42 +41,87 @@ public class GameController : MonoBehaviour
         healthBar.SetMaxHealth(playerMaxSanity);
 
         curDoor = numDoors;
+
+        blockDoorsCollider.gameObject.SetActive(false);
+
+        doorsLeft.text = numDoors + " Doors Remaining";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerCurrentSanity <= 1)
+        if (!currentlyEnding)
         {
-            Debug.Log("Game Over");
-        }
+            if (playerCurrentSanity <= 1)
+            {
+                currentlyEnding = true;
 
-        if (curDoor == 0)
-        {
-            Debug.Log("Player Wins");
+                Debug.Log("Game Over");
+
+                outcomeText.gameObject.SetActive(true);
+                fadingToBlack.color = new Color(fadingToBlack.color.r, fadingToBlack.color.g, fadingToBlack.color.b, 100f);
+
+                outcomeText.text = "You've lost your mind...";
+                damageDesc.text = "Sanity is 0";
+
+                audioSource.PlayOneShot(audioClips[7]);
+                audioSource.PlayOneShot(audioClips[8]);
+
+                Invoke("GoToMainMenu", 4);
+            }
+
+            if (curDoor == 0)
+            {
+                currentlyEnding = true;
+
+                Debug.Log("Player Wins");
+
+                outcomeText.gameObject.SetActive(true);
+                fadingToBlack.color = new Color(fadingToBlack.color.r, fadingToBlack.color.g, fadingToBlack.color.b, 100f);
+
+                outcomeText.text = "You've escaped... for now";
+                damageDesc.text = "Final Sanity is " + playerCurrentSanity;
+
+                audioSource.PlayOneShot(audioClips[6]);
+                audioSource.PlayOneShot(audioClips[8]);
+
+
+                Invoke("GoToMainMenu", 4);
+
+            }
         }
+        
     }
 
     private void endSet()
     {
+
         outcomeText.gameObject.SetActive(true);
+        damageDesc.gameObject.SetActive(true);
         fadingToBlack.color = new Color(fadingToBlack.color.r, fadingToBlack.color.g, fadingToBlack.color.b, 100f);
 
-        if (outcome.name.Equals("Monster"))
+        if (outcome.name.Equals("Monster(Clone)"))
         {
+
             outcomeText.text = "MONSTER";
-            audioSource.PlayOneShot(audioClips[0]);
-            healthBar.SetHealth(playerCurrentSanity - monsterDamage);
+            damageDesc.text = "-20 Sanity";
+            playerCurrentSanity -= monsterDamage;
+            healthBar.SetHealth(playerCurrentSanity);
+
+            if (playerCurrentSanity > 1)
+                audioSource.PlayOneShot(audioClips[0]);
         }
-        if (outcome.name.Equals("Item"))
+        if (outcome.name.Equals("Item(Clone)"))
         {
             outcomeText.text = "ITEM";
+            damageDesc.text = "-X Sanity";
             audioSource.PlayOneShot(audioClips[1]);
             Debug.Log("ITEM");
         }
-        if (outcome.name.Equals("Safety"))
+        if (outcome.name.Equals("Safety(Clone)"))
         {
             outcomeText.text = "SAFETY";
+            damageDesc.text = "Sanity Unchanged";
             audioSource.PlayOneShot(audioClips[2]);
             Debug.Log("SAFETY");
         }
@@ -79,8 +129,8 @@ public class GameController : MonoBehaviour
         curDoor -= 1;
         doorsLeft.text = curDoor + " Doors Remaining";
 
-        audioSource.PlayOneShot(audioClips[5]);
 
+        audioSource.PlayOneShot(audioClips[5]);
         Invoke("ResetScene", 4);
 
     }
@@ -88,10 +138,24 @@ public class GameController : MonoBehaviour
     private void ResetScene()
     {
         outcomeText.gameObject.SetActive(false);
+        damageDesc.gameObject.SetActive(false);
         GameObject.Find(currentDoor).transform.parent.Rotate(new Vector3(0, -80, 0));
         fadingToBlack.color = new Color(fadingToBlack.color.r, fadingToBlack.color.g, fadingToBlack.color.b, 0f);
         audioSource.PlayOneShot(audioClips[4]);
+        Destroy(outcome);
 
+        Invoke("ResetCollider", 3);
+    }
+
+    private void ResetCollider()
+    {
+        blockDoorsCollider.gameObject.SetActive(false);
+
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void currentOpeningDoor(string doorName)
@@ -106,12 +170,31 @@ public class GameController : MonoBehaviour
         GameObject.Find(currentDoor).transform.parent.Rotate(new Vector3(0, 80, 0));
 
         outcome = chooseOutcome();
+        Debug.Log(outcome);
+
+        itemName = outcome;
 
         GameObject spawnLoc = GetSpawn();
 
-        Instantiate(outcome, spawnLoc.transform.position, Quaternion.identity);
+        outcome = Instantiate(outcome, spawnLoc.transform.position, Quaternion.identity);
+
+        if (outcome.gameObject.name.Equals("Monster(Clone)"))
+        {
+            outcome.transform.Rotate(new Vector3(0, 90, 0));
+            outcome.transform.Translate(new Vector3(.5f, -.5f, 0));
+        }
+        if (outcome.gameObject.name.Equals("Safety(Clone)"))
+        {
+            //outcome.transform.Rotate(new Vector3(180, 0, 0));
+            outcome.transform.Translate(new Vector3(0, 0, -.5f));
+        }
+
+        Debug.Log(outcome.gameObject.name);
 
         audioSource.PlayOneShot(audioClips[3]);
+
+        blockDoorsCollider.gameObject.SetActive(true);
+
 
         Invoke("endSet", 2f);
     }
@@ -143,7 +226,7 @@ public class GameController : MonoBehaviour
         {
             return item;
         }
-        else if (rand < 90)
+        else if (rand <= 90)
         {
             return safety;
         }
